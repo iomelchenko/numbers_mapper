ec5a98d520a924072bf3f035f7c143ebe0a7ee7c
Igor Omelchenko <igomelch@gmail.com>
2017-06-03 17:56:51 +0300

    Implemented fetching pair combinations

--- app/mapping_generator.rb
+++ app/mapping_generator.rb
@@ -8,7 +8,9 @@ class MappingGenerator
   end
 
   def execute
-    phone_number
+    pairs = fetch_words(pair_combinations)
+    puts pairs.to_s
+    pairs
   end
 
   private
@@ -17,5 +19,36 @@ class MappingGenerator
     Marshal.load File.read index_file_path
   rescue => error
     puts "#{error}. Please create index file"
+  end
+
+  def pair_combinations
+    (3..7).map do |first_el|
+      first_el.to_s + (10 - first_el).to_s
+    end
+  end
+
+  def fetch_words(combinations)
+    all_words = []
+    combinations.each do |el|
+      words_for_number = []
+      start_idx = 0
+      length = el.split('').map(&:to_i)
+      fetch_for_number_part(length, words_for_number, start_idx)
+
+      unless words_for_number.empty? || words_for_number.length < length.count
+        all_words << words_for_number
+      end
+    end
+
+    all_words
+  end
+
+  def fetch_for_number_part(length, words_for_number, start_idx)
+    length.each do |len|
+      words_for_part_number = index[phone_number[start_idx, len].to_i]
+      break unless words_for_part_number
+      words_for_number << words_for_part_number
+      start_idx += len
+    end
   end
 end
