c5050aeab449485ec343627e94bdff59231d47f3
Igor Omelchenko <igomelch@gmail.com>
2017-06-03 14:27:22 +0300

    Initial commit

---
+++ app/initializer.rb
@@ -0,0 +1,4 @@
+require 'pry'
+require './app/mapping_generator'
+
+DICTIONARY_FILE_PATH = './files/dictionary.txt'
---
+++ app/mapping_generator.rb
@@ -0,0 +1,12 @@
+class MappingGenerator
+  attr_reader :file_path, :phone_number
+
+  def initialize(phone_number)
+    @file_path    = DICTIONARY_FILE_PATH
+    @phone_number = phone_number
+  end
+
+  def execute
+    puts phone_number
+  end
+end
